7502f7043fa6a28262b29f6c15553115e3361aca
svitlana-p <sweetlana.pukish@gmail.com>
2020-05-02 15:14:29 +0000

    javascript intro

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-alert('Hello World');
+//alert('Hello');
+
+let greetings='World!';
+
+//alert(greetings);
+
+const message='Hello ' + greetings;
+alert(message);
+const userAge=parseFloat(prompt('Enter your age'));
+alert('Your age is ' + userAge);
+
+const nextAge=add (userAge, 1);
+alert (nextAge);
+if (userAge >=18) {
+alert ('You are adult')
+}
+else {
+alert ('You are anderaged')
+}
+
+function add (a, b) {
+    const result= a+b;
+    return result;
+}
